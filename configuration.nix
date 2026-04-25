2b37e5fc649a60a1ed2ddad05aecb5f23708ccbd
tkclam <thomas.lam@epfl.ch>
2026-04-25 16:02:52 +0200

    Add OpenConnect to system packages and enable experimental Nix features

--- configuration.nix
+++ configuration.nix
@@ -106,6 +106,7 @@
     tmux
     vscode
     uv
+    openconnect
   ];
 
   programs.nix-ld.enable = true;
@@ -148,6 +149,8 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+  
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
